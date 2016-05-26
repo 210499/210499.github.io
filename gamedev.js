4b74ca7bc96d578df4205206618ab15db1392009
210499 <210499@hkis.edu.hk>
2016-05-26 11:08:45 +0800

    almost done

--- gamedev.js
+++ gamedev.js
@@ -1,5 +1,48 @@
-$( document ).ready(function() {
+$( document ).ready(function(){
+var mouseX = 0, mouseY = 0, limitX = 1305.450-15, limitY = 200-15;
+$(window).mousemove(function(e){
+  var offset = $('.container').offset();
+   mouseX = Math.min(e.pageX - offset.left, limitX);
+   mouseY = Math.min(e.pageY - offset.top, limitY);
+   if (mouseX < 0) mouseX = 0;
+   if (mouseY < 0) mouseY = 0;
+});
 
-//Your jquery goes here
+// cache the selector
+var follower = $("#follower");
+var xp = 0, yp = 0;
+var loop = setInterval(function(){
+    // change 12 to alter damping higher is slower
+    xp += (mouseX - xp) / 12;
+    yp += (mouseY - yp) / 12;
+    follower.css({left:xp, top:yp});
+    
+}, 30);
+$("#slime").show();
+
+animateDiv("#slime");
+function makeNewPosition(){
+    
+    // Get viewport dimensions (remove the dimension of the div)
+    var h = $(window).height() - 50;
+    var w = $(window).width() - 50;
+    
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+    
+    return [nh,nw];    
+    
+}
+
+function animateDiv(myclass){
+    var newq = makeNewPosition();
+    $(myclass).animate({ top: newq[0], left: newq[1] }, 1000,   function(){
+      animateDiv(myclass);        
+    });
+    
+};
+$("#slime").click(function(){
+   $("#slime").hide().delay(2000);
 
 });
+});//closes doc
